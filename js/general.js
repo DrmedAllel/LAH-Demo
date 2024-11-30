c3dcbfbe18c5f51b6776ea755100c6cf1d842776
DrmedAllel <claudius.caspar.laur@gmail.com>
2024-11-30 10:36:24 +0100

    Aktualisiere Links zu Flugzeug-Bewaffnung und Flugzeug-Ausrüstung mit neuen Pfaden

--- js/general.js
+++ js/general.js
@@ -50,11 +50,11 @@ document.addEventListener('DOMContentLoaded', function() {
             </div>
 
             <div class="dropdown">
-            <a class="title" href="/LAH-Demo/flugzeug_bewaffnung.html">Flugzeug-Bewaffnung</a>
+            <a class="title" href="/LAH-Demo/Zusatz/flugzeug_bewaffnung.html">Flugzeug-Bewaffnung</a>
             </div>
 
             <div class="dropdown">
-            <a class="title" href="/LAH-Demo/flugzeug_ausruestung.html">Flugzeug-Ausrüstung</a>
+            <a class="title" href="/LAH-Demo/Zusatz/flugzeug_ausruestung.html">Flugzeug-Ausrüstung</a>
             </div>
 
             <div class="dropdown">
