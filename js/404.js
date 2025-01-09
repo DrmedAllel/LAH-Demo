2aaf6d62410cf56cce1392dc7d08965ea76284d8
DrmedAllel <claudius.caspar.laur@gmail.com>
2025-01-09 21:43:46 +0100

    Füge eine benutzerdefinierte 404-Seite hinzu mit einem Button, um zur Startseite zurückzukehren.

--- js/404.js
+++ js/404.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', function() {
     mainSection.innerHTML = `
         <h1>404 - Page Not Found</h1>
         <p>${language === 'de' ? 'Die Seite, die Sie suchen, konnte nicht gefunden werden.' : 'The page you are looking for could not be found.'}</p>
+        <button id='back_to_shop' class='button' onclick="window.location.href='index.html'">${language === 'de' ? 'Zurück zur Startseite' : 'Back to Homepage'}</button>
     ` + mainSection.innerHTML;
 });
 
